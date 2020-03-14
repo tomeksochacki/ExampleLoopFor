0e3023a572d966d67cda921916ed3a49860357bc
tomek <tomeksochacki@wp.pl>
2020-03-14 15:23:38 +0100

    Initialized repository

---
+++ src/ExampleLoopFor.java
@@ -0,0 +1,20 @@
+public class ExampleLoopFor {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i<=100; i++){
+            sum = i + sum;
+        }
+        System.out.println("Sum all numbers one to hundred is: " + sum);
+
+
+
+
+
+
+
+    }
+
+
+}
